32af4c688ef64f47ae83f2acbe95c15dc7a50462
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2024-08-28 11:53:30 +0200

    Fett mycket

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-document
-  .getElementById("containerButton")
-  .addEventListener("click", handleEvent);
+document.addEventListener("DOMContentLoaded", function () {
+  const containerButton = document.querySelector(".containerButton");
+  const containerForm = document.querySelector(".containerForm");
+  const addButton = document.querySelector(".addButton");
+  const ingredientInput = document.getElementById("ingredientInput");
+  const amountInput = document.getElementById("amountInput");
+  const dropDown = document.getElementById("dropDown");
 
-function handleEvent() {
-    
-}
+  let ingredients = [];
+
+  containerForm.style.display = "block";
+  containerButton.addEventListener("click", function () {
+    if (
+      containerForm.style.display === "none" ||
+      containerForm.style.display === ""
+    ) {
+      containerForm.style.display = "block";
+    } else {
+      containerForm.style.display = "none";
+    }
+  });
+
+  addButton.addEventListener("click", function () {
+    const ingridient = ingredientInput.value.trim();
+    const unit = dropDown.value;
+    const amount = amountInput.value.trim();
+
+    if (ingridient && unit && amount) {
+      const ingredientObject = {
+        name: ingridient,
+        unit: unit,
+        amount: amount,
+      };
+
+      ingredients.push(ingredientObject);
+      ingredientInput.value = "";
+      amountInput.value = "";
+    }
+  });
+});
